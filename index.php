dff8d150379b916502e65fe5027cf3c4115818a9
McGilfordJose <chucho970407@gmail.com>
2020-07-09 01:20:43 -0500

    ok

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title> 
+     <!--JQUERY-->
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+     <!-- FRAMEWORK BOOTSTRAP para el estilo de la pagina-->
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+     <!-- Los iconos tipo Solid de Fontawesome-->
+     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">
+     <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+     <!-- Nuestro css-->
+     <link rel="stylesheet" type="text/css" href="css/main.css">
+     <meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8″ />
+     <script xmlns="http://www.w3.org/1999/xhtml" src="jquery/jquery-3.2.1.js"></script>
+</head>
+<body class="login">
+<div class="login-image">
+    hola como estas amigo como estas bien amigo espa;o
+    como estas l6
+    yo
+    s bien y tu Xd
+    xd
+    yo bien
+</div>
+<footer>
+</footer>
+</body>
+</html>
